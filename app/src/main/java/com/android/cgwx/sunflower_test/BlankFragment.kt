9691439565bac4cd83b1349630fec23ebc4fa3c6
lixxxiang <13051744716@163.com>
2020-08-28 14:32:44 +0800

    work

--- app/src/main/java/com/android/cgwx/sunflower_test/BlankFragment.kt
+++ app/src/main/java/com/android/cgwx/sunflower_test/BlankFragment.kt
@@ -1,18 +1,10 @@
 package com.android.cgwx.sunflower_test
 
 import android.os.Bundle
-import android.util.Log
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import androidx.fragment.app.viewModels
-import androidx.lifecycle.ViewModel
-import androidx.lifecycle.observe
-import com.android.cgwx.sunflower_test.adapter.PlantAdapter
-import com.android.cgwx.sunflower_test.databinding.FragmentPlantListBinding
-import com.android.cgwx.sunflower_test.utilities.InjectorUtils
-import com.android.cgwx.sunflower_test.viewmodels.PlantListViewModel
 
 // TODO: Rename parameter arguments, choose names that match
 // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
@@ -21,10 +13,10 @@ private const val ARG_PARAM2 = "param2"
 
 /**
  * A simple [Fragment] subclass.
- * Use the [PlantListFragment.newInstance] factory method to
+ * Use the [BlankFragment.newInstance] factory method to
  * create an instance of this fragment.
  */
-class PlantListFragment : Fragment() {
+class BlankFragment : Fragment() {
     // TODO: Rename and change types of parameters
     private var param1: String? = null
     private var param2: String? = null
@@ -37,33 +29,12 @@ class PlantListFragment : Fragment() {
         }
     }
 
-    private val viewModel: PlantListViewModel by viewModels {
-        InjectorUtils.providePlantListViewModelFactory(this)
-    }
-
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
         // Inflate the layout for this fragment
-        val binding = FragmentPlantListBinding.inflate(inflater, container, false)
-        context ?: return binding.root
-
-        val adapter = PlantAdapter()
-        binding.plantList.adapter = adapter
-        subscribeUi(adapter)
-
-        setHasOptionsMenu(true)
-        return binding.root
-    }
-
-    fun subscribeUi(adapter: PlantAdapter){
-        viewModel.plants.observe(viewLifecycleOwner) {
-            plants ->
-            var p = plants
-            var a = adapter
-            adapter.submitList(plants)
-        }
+        return inflater.inflate(R.layout.fragment_blank, container, false)
     }
 
     companion object {
@@ -73,12 +44,12 @@ class PlantListFragment : Fragment() {
          *
          * @param param1 Parameter 1.
          * @param param2 Parameter 2.
-         * @return A new instance of fragment PlantListFragment.
+         * @return A new instance of fragment BlankFragment.
          */
         // TODO: Rename and change types and number of parameters
         @JvmStatic
         fun newInstance(param1: String, param2: String) =
-            PlantListFragment().apply {
+            BlankFragment().apply {
                 arguments = Bundle().apply {
                     putString(ARG_PARAM1, param1)
                     putString(ARG_PARAM2, param2)
